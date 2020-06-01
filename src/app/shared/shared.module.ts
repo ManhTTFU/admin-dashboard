cdbd0eeef934bc0e5c5c1f1be50d6d2d4a2af534
Manh Tran <User@WV1ISC9090.officehn.local>
2020-06-01 17:58:12 +0700

    create layout and setup environment

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { HighchartsChartModule } from "highcharts-angular";
+import { RouterModule } from "@angular/router";
+import { AreaComponent } from "./widgets/area/area.component";
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    AreaComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule,
+    HighchartsChartModule,
+    RouterModule,
+  ],
+  exports: [HeaderComponent, FooterComponent, SidebarComponent, AreaComponent],
+})
+export class SharedModule {}
